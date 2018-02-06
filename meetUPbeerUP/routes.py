544893123a5166aba2d3daa9f12e893916a88b84
Konnarm <suchecki.konrad@hotmail.com>
2018-02-06 21:53:17 +0100

    added base template and react

--- meetUPbeerUP/routes.py
+++ meetUPbeerUP/routes.py
@@ -1,6 +1,8 @@
+from flask import render_template
 from meetUPbeerUP import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('index.html', title='Meet UP beer UP!')
